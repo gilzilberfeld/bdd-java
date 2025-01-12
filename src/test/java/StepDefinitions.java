b0c3f5373bef668f769d6cda5d1922e5af778833
gilzilberfeld <gil.zilberfeld@gmail.com>
2025-01-12 11:10:23 +0200

    initial

---
+++ src/test/java/StepDefinitions.java
@@ -0,0 +1,19 @@
+import bdd.examples.d01.Calculator;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StepDefinitions {
+    Calculator calc;
+
+    @Given("calculator was just turned on")
+    public void calculator_was_just_turned_on() {
+         calc = new Calculator();
+    }
+    @Then("It shows zero")
+    public void it_shows_zero() {
+        assertThat(calc.getDisplay()).isEqualTo("0");
+    }
+
+}
---
+++ src/test/resources/bdd-scenarios/d01.simple.feature
@@ -0,0 +1,5 @@
+Feature: Calculator startup
+
+  Scenario: On start-up the display should show zero
+    Given calculator was just turned on
+    Then It shows zero
